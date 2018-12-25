11f35add9b3c1de46790a9b1123ec1a7b5dd15e7
alexanderwiederin <alexander.wiederin@gmail.com>
2018-12-25 13:34:10 +0100

    completed capitalize

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,38 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const anagrams = (stringA, stringB) => {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+const cleanString = string => string
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+
+
 
 module.exports = anagrams;
+
+// const anagrams = (stringA, stringB) => {
+//   const stringBMap = buildCharMap(stringA);
+//   const stringAMap = buildCharMap(stringB);
+//   if (Object.keys(stringBMap).length !== Object.keys(stringAMap).length) {
+//     return false;
+//   }
+//   for (let character in stringAMap) {
+//     if (stringAMap[character] !== stringBMap[character]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// const buildCharMap = (str) => {
+//   return str.replace(/[^\w]/g, '').split('').reduce((acc, character) => {
+//     acc[character.toLowerCase()] = acc[character.toLowerCase()] + 1 || 1;
+//     return acc;
+//   }, {});
+// }
--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,11 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+const capitalize = (str) => {
+  return str.split(' ').map((word) => {
+    const firstLetter = word[0].toUpperCase();
+    return firstLetter + word.slice(1);
+  }).join(' ');
+}
 
 module.exports = capitalize;
