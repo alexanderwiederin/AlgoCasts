3ac2de0357acad32e8efcd644c65b3321abc694b
alexanderwiederin <alexander.wiederin@gmail.com>
2018-12-25 15:16:33 +0100

    completed steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,31 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// const steps = (n) => {
+//   for (let i = 1; i <= n; i += 1) {
+//     let step = '';
+//     for (let j = 1; j <= n; j += 1) {
+//       if (j <= i) {
+//         step += '#';
+//       } else {
+//         step += ' ';
+//       }
+//     }
+//     console.log(step);
+//   }
+// }
+
+const steps = (n, row = 0, stair = '') => {
+  if (n === row) {
+    return;
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+  const add = stair.length <= row ? '#' : ' ';
+  steps(n, row, stair + add);
+}
 
 module.exports = steps;
