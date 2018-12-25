0abd6e9e23250f58615efdfaedea9227f062c4cf
alexanderwiederin <alexander.wiederin@gmail.com>
2018-12-25 12:21:35 +0100

    completed anagrams

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,21 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+const chunk = (array, size) => {
+  const result = [];
+  const recursiveFunction = (leftArray, subarray = []) => {
+    subarray.push(leftArray[0]);
+    if (leftArray.length === 1) {
+      result.push(subarray);
+    } else if (subarray.length === size) {
+      result.push(subarray);
+      recursiveFunction(leftArray.slice(1));
+    } else {
+      recursiveFunction(leftArray.slice(1), subarray);
+    }
+  };
+  recursiveFunction(array);
+  return result;
+};
 
 module.exports = chunk;
